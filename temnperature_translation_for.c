104aee28e2e9b952d26ba9f5eaa02480561af174
EgorSmolenov21 <130052886+EgorSmolenov21@users.noreply.github.com>
2023-04-09 15:28:03 +0300

    2.0

--- temnperature_translation_for.c
+++ temnperature_translation_for.c
@@ -2,7 +2,7 @@
 
 int main() {
     int fahr;
-    for(fahr = 0; fahr <= 300; fahr += 20) {
+    for(fahr = 0; fahr >= 0; fahr += 20) {
         printf("fahr: %3d\t celsius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
 }
